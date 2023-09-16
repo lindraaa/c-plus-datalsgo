739fef57ca22e8f37a9fd5145ed230467cc3555a
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-16 21:03:31 +0800

    Create dynamicsize.cpp

---
+++ dynamicsize.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+	std::vector<int> data{1, 2, 3, 4};
+	data.push_back(5);
+	data.push_back(6);
+	data.push_back(7);
+	data.push_back(8);
+	data.push_back(9);
+	cout << "size = " << data.size();
+ /* Adding the size is use the push_back function
+  	and I use data.size to get the length of 
+  	the arrays has been added */
+}
