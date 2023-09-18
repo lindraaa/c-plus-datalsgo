60c49ef1a821400ac03c21167939ada1778f4b18
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-18 22:15:07 +0800

    Create pointers.cpp

---
+++ pointers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int luckynumbers[5];//={2,3,5,7,9};
+	/*cout << luckynumbers[0] <<endl;
+	cout << *(luckynumbers+ 2);*/
+	
+	for (int i = 0 ; i<=4; i++)
+	{
+		cout << "Number: ";
+		cin >>luckynumbers[i];
+	}
+		for (int i = 0 ; i<=4; i++)
+	{
+		
+		cout << *(luckynumbers+i) << " ";
+ 	}
+		
+}
