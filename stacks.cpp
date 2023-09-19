e3fad91e0b521cd65edc1b6503759dd992e9c901
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-19 18:29:23 +0800

    Create stacks.cpp

---
+++ stacks.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+ 
+void printStackelements(stack<int> stack){
+  while(!stack.empty()){
+      cout << stack.top() << " | ";
+        stack.pop();
+        
+  }
+}
+void deletestackelemnts(stack<int> stack){
+    stack.pop();
+    cout <<"\nThe new stack size is " << stack.size() <<endl;
+  while(!stack.empty()){
+    cout << stack.top() <<" | ";
+      stack.pop();
+  }
+}
+
+int main()
+{
+    // empty , size, push, pop- means erase, top
+    stack<int> numbersStack;
+    numbersStack.push(24);
+    numbersStack.push(11);
+    numbersStack.push(16);
+    numbersStack.push(31);
+    numbersStack.push(21);
+    numbersStack.push(27);
+    
+    if(numbersStack.empty()){
+        cout <<"Stack is empty!! ";
+    }
+    else{
+        cout <<"Stack is filled ";
+    }
+
+    cout <<"\nStack size is " << numbersStack.size()<<endl;
+    
+    printStackelements(numbersStack);
+    deletestackelemnts(numbersStack);
+     
+}
